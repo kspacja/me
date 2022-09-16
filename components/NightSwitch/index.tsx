f682030b5fd3f6242a356b03586891113394b3f8
Chris <k.ciach@dook.pro>
2022-09-17 00:39:14 +0200

    Update accessebility

--- components/NightSwitch/index.tsx
+++ components/NightSwitch/index.tsx
@@ -69,6 +69,9 @@ export default function NightSwitch() {
             return state === "day" ? "night" : "day";
           });
         }}
+        aria-label={
+          defferTimeOfDay === "day" ? "Switch to night" : "Switch to day"
+        }
       >
         {defferTimeOfDay === "day" ? (
           <Sun width="30" height="30" />
