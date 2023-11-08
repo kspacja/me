adf14b65d8ae5aadb4d8596f2d15aee84b53808b
Chris <k.ciach@dook.pro>
2023-11-08 15:06:38 +0100

    Update svgs

--- components/NightSwitch/index.tsx
+++ components/NightSwitch/index.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { useDeferredValue, useEffect, useRef, useState } from "react";
-// import Sun from "./sun.svg";
-// import Moon from "./moon.svg";
+import Sun from "./sun.svg";
+import Moon from "./moon.svg";
 
 import { Button } from "./styles";
 
@@ -60,9 +60,9 @@ export default function NightSwitch() {
   }, []);
 
   useEffect(() => {
-    // if (defferTimeOfDay) {
-    //   document.body.classList.add(defferTimeOfDay);
-    // }
+    if (defferTimeOfDay) {
+      document.body.className = defferTimeOfDay;
+    }
 
     setFavicon(defferTimeOfDay);
   }, [defferTimeOfDay]);
@@ -84,11 +84,11 @@ export default function NightSwitch() {
           defferTimeOfDay === "day" ? "Switch to night" : "Switch to day"
         }
       >
-        {/* {defferTimeOfDay === "day" ? (
+        {defferTimeOfDay === "day" ? (
           <Sun width="30" height="30" />
         ) : (
           <Moon width="30" height="30" />
-        )} */}
+        )}
       </Button>
     </>
   );
