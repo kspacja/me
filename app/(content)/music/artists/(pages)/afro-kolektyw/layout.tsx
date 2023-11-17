a84faa3d97e426221ebef1ca5ab256c516e1dab1
Chris <k.ciach@dook.pro>
2023-11-17 23:43:24 +0100

    Afro Kolektyw page

---
+++ app/(content)/music/artists/(pages)/afro-kolektyw/layout.tsx
@@ -0,0 +1,8 @@
+import PassThrough from "components/PassThrough";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Afro Kolektyw",
+};
+
+export default PassThrough;
