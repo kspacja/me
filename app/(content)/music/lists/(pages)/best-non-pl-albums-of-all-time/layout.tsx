19ae41743cd8f03dbd60d36c6b11e88fa72d82de
Chris <k.ciach@dook.pro>
2023-11-21 01:05:32 +0100

    Update og image

--- app/(content)/music/lists/(pages)/best-non-pl-albums-of-all-time/layout.tsx
+++ app/(content)/music/lists/(pages)/best-non-pl-albums-of-all-time/layout.tsx
@@ -2,7 +2,7 @@ import PassThrough from "components/PassThrough";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "",
+  title: "Best non-PL albums of all time",
 };
 
 export default PassThrough;
