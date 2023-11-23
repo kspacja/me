6af4e6b6be3e913ff95e23457f800f20cc7ee932
Chris <k.ciach@dook.pro>
2023-11-23 01:48:49 +0100

    PL Album of all time

---
+++ app/(content)/music/lists/(pages)/best-pl-albums-of-all-time/layout.tsx
@@ -0,0 +1,8 @@
+import PassThrough from "components/PassThrough";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Best Polish Albums of All Time",
+};
+
+export default PassThrough;
