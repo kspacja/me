70ccb56c88b2e997500d4bacaba6d5ec7ce3913c
Chris <k.ciach@dook.pro>
2023-11-25 01:00:28 +0100

    Best non-pl singles of all time

---
+++ app/(content)/music/lists/(pages)/best-non-pl-repeats-of-all-time/layout.tsx
@@ -0,0 +1,8 @@
+import PassThrough from "components/PassThrough";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Best non-PL Repeats of All Time",
+};
+
+export default PassThrough;
