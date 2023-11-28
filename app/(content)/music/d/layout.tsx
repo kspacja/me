7c217b3a459a0cc018b9d4ccb695d098f0a60d0d
Chris <k.ciach@dook.pro>
2023-11-28 18:02:48 +0100

    Add temporary link

---
+++ app/(content)/music/d/layout.tsx
@@ -0,0 +1,8 @@
+import PassThrough from "components/PassThrough";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Link and temporary things",
+};
+
+export default PassThrough;
