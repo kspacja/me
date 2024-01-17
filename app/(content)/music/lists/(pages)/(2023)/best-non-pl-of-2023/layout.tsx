093058d109ee823cf6f6b3ad9c0816e4e994372d
Chris <k.ciach@dook.pro>
2024-01-17 21:47:29 +0100

    2023 update

---
+++ app/(content)/music/lists/(pages)/(2023)/best-non-pl-of-2023/layout.tsx
@@ -0,0 +1,8 @@
+import PassThrough from "components/PassThrough";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Best non-PL 2023",
+};
+
+export default PassThrough;
---
+++ app/(content)/music/lists/(pages)/(all-time)/best-classical-pieces-of-all-time/layout.tsx
@@ -0,0 +1,8 @@
+import PassThrough from "components/PassThrough";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Best Classical Music of All Time",
+};
+
+export default PassThrough;
