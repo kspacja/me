a5a37b8998c93e75265eeb3992e1dc0371e8bbec
Chris <k.ciach@dook.pro>
2024-05-23 09:38:28 +0200

    feat: update seo pt. 2

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,5 +3,5 @@ module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://ciachu.site",
   generateRobotsTxt: true,
   changefreq: "monthly",
-  // generateIndexSitemap: false,
+  generateIndexSitemap: false,
 };
