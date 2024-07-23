58604173fefe3a15f8a5ef3919b289e838e10493
Chris <k.ciach@dook.pro>
2024-07-23 14:00:08 +0200

    update content

--- app/(content)/music/artists/(pages)/tatsuro-yamashita/layout.tsx
+++ app/(content)/music/artists/(pages)/tatsuro-yamashita/layout.tsx
@@ -2,7 +2,7 @@ import PassThrough from "components/PassThrough";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Rycerzyki",
+  title: "Tatsuro Yamashita",
 };
 
 export default PassThrough;
