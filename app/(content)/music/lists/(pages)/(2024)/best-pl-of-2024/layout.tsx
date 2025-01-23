ee41c7205db92af2eb6a96867df3c0f32d049b25
Krzysztof Ciach <kspacja@gmail.com>
2025-01-23 21:01:45 +0100

    2024 update

---
+++ app/(content)/music/lists/(pages)/(2024)/best-pl-of-2024/layout.tsx
@@ -0,0 +1,8 @@
+import PassThrough from "components/PassThrough";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Best PL 2024",
+};
+
+export default PassThrough;
