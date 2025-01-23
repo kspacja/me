c54524294d8cd992f7a9504d2452063f5fc3144e
Krzysztof Ciach <kspacja@gmail.com>
2025-01-23 21:53:57 +0100

    Update Jamiroquai layout title and add Steely Dan layout and page

--- app/(content)/music/artists/(pages)/steely-dan/layout.tsx
+++ app/(content)/music/artists/(pages)/steely-dan/layout.tsx
@@ -2,7 +2,7 @@ import PassThrough from "components/PassThrough";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Jamiroquai",
+  title: "Steely Dan",
 };
 
 export default PassThrough;
