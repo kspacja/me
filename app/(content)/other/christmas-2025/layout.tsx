d84ebb57bd60679706c16e2b2df97a2b6fb896ba
Krzysztof Ciach <kspacja@gmail.com>
2025-12-09 22:38:31 +0100

    Add layout and page for "Christmas 2025"

---
+++ app/(content)/other/christmas-2025/layout.tsx
@@ -0,0 +1,8 @@
+import PassThrough from "components/PassThrough";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Christmas 2025",
+};
+
+export default PassThrough;
