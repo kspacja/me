9b6ddd3d7375e603f583a52153629943363764a1
Krzysztof Ciach <kspacja@gmail.com>
2026-04-16 22:50:17 +0200

    Add layout and content for best Polish songwriters appendix page

---
+++ app/(content)/music/lists/(pages)/(all-time)/best-polish-songwriters-appendix/layout.tsx
@@ -0,0 +1,26 @@
+import PassThrough from "components/PassThrough";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Najlepsi polscy piosenkopisarze i pisarki - appendix",
+  description:
+    "Appendix do listy moich ulubionych polskich piosenkopisarzy i pisarek,",
+  keywords: [
+    "polska",
+    "piosenkopisarze",
+    "piosenkarki",
+    "songwriters",
+    "polska piosenka",
+    "piosenki",
+  ],
+  openGraph: {
+    siteName: "Krzysztof Ciach - Resume page",
+    type: "website",
+    title: "Najlepsi polscy piosenkopisarze i pisarki - appendix",
+  },
+  alternates: {
+    canonical: "/music/lists/best-polish-songwriters-appendix",
+  },
+};
+
+export default PassThrough;
